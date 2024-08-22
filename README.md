32f0d74f287a519dc4e665f38b1bfc9a96009337
zfx-code <1321772317@qq.com>
2024-08-22 10:05:04 +0800

    update shared_ptr.cpp

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # My-SmartPointers
 Implementation of Thread-Safe Smart Pointers
+
+## 1. SharedPointer
+
+Refer to [`shared_ptr.cpp`](./shared_ptr.cpp).
+
+## 2. UniquePointer
+
+To be continued...
---
+++ shared_ptr.cpp
@@ -0,0 +1,127 @@
+#include <iostream>
+#include <cassert>
+#include <mutex>
+using namespace std;
+
+class Person {
+public:
+    string name;
+    int age;
+    Person() {
+        name = "testName";
+        age = 18;
+        cout << "调用构造函数" << endl;
+    }
+    ~Person() {
+        cout << "调用析构函数" << endl;
+    }
+};
+
+// 指向所有类型的智能指针(使用模板类)
+template<class T>
+class SharedPointer {
+private:
+    T* _ptr;
+    // 共享引用计数器
+    size_t* _count;
+    // 共享锁
+    mutex* _mutex;
+    // 封装引用计数器操作, 使用锁保证线程安全
+    void addCount() {
+        _mutex->lock();
+        (*_count)++;
+        _mutex->unlock();
+    }
+
+    void deleteCount() {
+        // 标记是否释放锁, 解锁后释放
+        bool deleteMutex = false;
+        _mutex->lock();
+        (*_count)--;
+        if (*_count == 0) {
+            delete _ptr;
+            delete _count;
+            deleteMutex = true;
+            cout << "智能指针指向的对象已释放" << endl;
+        }
+        _mutex->unlock();
+        if (deleteMutex == true) {
+            delete _mutex;
+        }
+    }
+
+public:
+    // 构造函数, 指针初始化
+    SharedPointer(T* p = nullptr) {
+        _ptr = p;
+        _mutex = new mutex;
+        if (p == nullptr) {
+            _count = new size_t(0);
+        }
+        else {
+            _count = new size_t(1);
+        }
+    }
+    
+    // 拷贝构造函数
+    SharedPointer(SharedPointer& sp) {
+        _ptr = sp._ptr;
+        _count = sp._count;
+        _mutex = sp._mutex;
+        addCount();
+    }
+
+    // 运算符重载: "->", "*", "="
+    T* operator->() {
+        assert(_ptr != nullptr);
+        return _ptr;
+    }
+
+    T& operator*() {
+        assert(_ptr != nullptr);
+        return *_ptr;
+    }
+
+    SharedPointer& operator=(SharedPointer& sp) {
+        // 相同则不需要赋值
+        if (_ptr == sp._ptr) {
+            return *this;
+        }
+        // 1. 解决旧的
+        deleteCount();
+        // 2. 指向新的
+        _ptr = sp._ptr;
+        _count = sp._count;
+        _mutex = sp._mutex;
+        addCount();
+        return *this;
+    }
+
+    // 引用计数
+    size_t use_count() {
+        return *_count;
+    }
+
+    // 析构函数
+    ~SharedPointer() {
+        if (_ptr == nullptr) {
+            cout << "指针为空" << endl;
+            delete _count;
+            return;
+        }
+        deleteCount();
+    }
+};
+int main() {
+    // g++ -o main shared_ptr.cpp && ./main
+    SharedPointer<Person> sp(new Person());
+    cout << sp->name << endl;
+    cout << (*sp).age << endl;
+    cout << sp.use_count() << endl;
+    SharedPointer<Person> sp2(sp);
+    cout << sp2.use_count() << endl;
+    SharedPointer<Person> sp3(new Person());
+    sp3 = sp;
+    cout << sp3.use_count() << endl;
+    return 0;
+}
